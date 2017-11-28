450cb35c771a5e70a2935f4ddad074b03c783cf2
Andy Nguyen <andy.nguyen@wdc.com>
2017-11-27 16:41:47 -0800

    no bootstrap

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+const port = 5000;
+
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.engine('html', require('ejs').renderFile);
@@ -14,13 +16,20 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });
+
     socket.on('chat message', function(msg){
-        // console.log('message: ' + msg);
+        console.log('message: ' + msg);
         io.emit('chat message', msg);
     });
 });
 
-
+// app.listen(port, err => {
+//     console.log(`running server on port ${port}`);
+// });
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
